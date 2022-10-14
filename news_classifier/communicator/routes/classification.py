d1cbfb08b4616e5dc521308a41e9e71f14d7624a
Ihar Yazerski <ihar.yazerski@gmail.com>
2022-10-14 17:37:01 +0400

    implement endpoints for triggering classification and retrieving the results

--- news_classifier/communicator/routes/classification.py
+++ news_classifier/communicator/routes/classification.py
@@ -1,3 +1,36 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
+
+from news_classifier.communicator.scheme.classification import ClassifyRequest, ClassifyResponse, ClassificationResultResponse
+from news_classifier.common.processors import broker, db
 
 router = APIRouter()
+
+
+@router.post(
+    '',
+    summary='Trigger predicting which news resource the provided texts belongs to and return current request ID',
+    response_model=ClassifyResponse
+)
+def classify_many(request_body: ClassifyRequest):
+    response = ClassifyResponse()
+    broker.produce(
+        task_data={'request_id': response.request_id, **request_body.dict()},
+        task_name='classify_many',
+        queue='classification'
+    )
+    return response
+
+
+@router.get(
+    '/{request_id}',
+    summary='Retrieve the result of the request sent to /classification',
+    response_model=list[ClassificationResultResponse]
+)
+def classification_result(request_id: str):
+    result = db.read(collection='results', query={'request_id': request_id})
+    if not result:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    elif not result['ready']:
+        raise HTTPException(status_code=status.HTTP_425_TOO_EARLY)
+    else:
+        return result['predictions']
