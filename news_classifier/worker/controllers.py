7896fb0f5819c92e3d18ee8cc5fbfb099678789a
Ihar Yazerski <ihar.yazerski@gmail.com>
2022-10-16 21:54:22 +0400

    - remove redundant args and logger from worker controllers;
    - add docstrings with logic explanation;

--- news_classifier/worker/controllers.py
+++ news_classifier/worker/controllers.py
@@ -1,23 +1,24 @@
-import logging
-
 from celery.signals import task_prerun
 
 from news_classifier.common.processors import broker, db
 from news_classifier.worker.tasks import classification
 
-_logger = logging.getLogger(__name__)
-
 
 @task_prerun.connect
 def on_task_init(*_, **__):
+    """
+    Establish DB connection on task init. Needed for a proper mongodb connection management
+    during parallel celery tasks processing
+    """
+
     db.connect()
 
 
 @broker.app.task(name='classify_one', queue='classification')
-def classify_one(*args, **kwargs):
-    classification.classify_one(*args, **kwargs)
+def classify_one(**kwargs):
+    classification.classify_one(**kwargs)
 
 
 @broker.app.task(name='classify_many', queue='classification')
-def classify_many(*args, **kwargs):
-    classification.classify_many(*args, **kwargs)
+def classify_many(**kwargs):
+    classification.classify_many(**kwargs)
