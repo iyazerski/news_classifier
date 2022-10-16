b9dd95e7e28ebf2a1008e0d93d973cd2cec7a5b5
Ihar Yazerski <ihar.yazerski@gmail.com>
2022-10-16 21:51:20 +0400

    add simple docstring for API scheme

--- news_classifier/communicator/scheme/classification.py
+++ news_classifier/communicator/scheme/classification.py
@@ -5,6 +5,8 @@ from pydantic import BaseModel, Field, validator
 
 
 class ClassifyRequest(BaseModel):
+    """ Schema for POST /classification request body """
+
     texts: list[str] = Field(..., min_items=1, description='List of news texts')
     max_predictions_num: Optional[int] = Field(None, description='Number of predictions', ge=1)
 
@@ -14,9 +16,13 @@ class ClassifyRequest(BaseModel):
 
 
 class ClassifyResponse(BaseModel):
+    """ Schema for POST /classification response body """
+
     request_id: str = Field(default_factory=str(uuid4()))
 
 
 class ClassificationResultResponse(BaseModel):
+    """ Schema for GET /classification response body """
+
     label: str
     proba: float
